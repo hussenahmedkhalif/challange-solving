b57e72dcab2e01b3edb4b760b69492b273e11023
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-05-15 23:24:40 +0300

    studentgrade problem lab

---
+++ studentgrade/index.js
@@ -0,0 +1,28 @@
+// first varified the variables
+let  studentGrade = prompt();
+studentGrade = parseInt(studentGrade);
+let output;
+
+switch (true) {
+    case (studentGrade <=79 && studentGrade >=60):
+        output ="A";
+        break;
+        case (studentGrade <=60 && studentGrade >=49):
+            output ="B";
+            break;
+            case (studentGrade<=49 && studentGrade >=40):
+                output = "C";
+                break;
+                case (studentGrade<=40 && studentGrade >=30):
+                output ="D";
+                break;
+                case (studentGrade<=30 && studentGrade >=0):
+                    output = "E";
+                    break;
+
+
+    default:
+        console.log("invalid input");
+        break;
+}console.log(output);
+document.write(output)
